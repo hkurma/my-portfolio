7bb6a5adbc2aae7f07156280e34b5f140b66e0d7
Harish Kurma <hkurma0504@gmail.com>
2021-02-15 11:38:11 -0500

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,35 @@
 import * as React from "react";
+import Layout from "../components/layout";
+import { Link } from "gatsby";
+
+const styles = {
+    height: "100vh"
+};
 
 const NotFoundPage = () => {
-    return <h1>Page not found</h1>;
+    return (
+        <Layout>
+            <div
+                className="container d-flex justify-content-center align-items-center"
+                style={styles}
+            >
+                <div className="text-center">
+                    <h2>
+                        <i class="fas fa-exclamation-circle"></i>
+                    </h2>
+                    <h1 className="mb-5">Page not found</h1>
+                    <Link to="/">
+                        <button
+                            type="button"
+                            className="btn btn-outline-primary"
+                        >
+                            Return to Home
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </Layout>
+    );
 };
 
 export default NotFoundPage;
