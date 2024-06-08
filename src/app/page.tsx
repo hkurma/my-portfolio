bed14076287b4e00c57de6275fb4928fb0e06576
Harish Kurma <hkurma0504@gmail.com>
2024-06-08 03:30:44 -0400

    Change UI design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 const Home = () => {
   return (
-    <div className="w-screen h-screen flex flex-col gap-8 justify-center items-center">
-      <div className="text-6xl">Hello, I&apos;m Harish Kurma</div>
+    <div className="max-w-screen h-screen p-4 flex flex-col gap-4 justify-center items-center">
+      <div className="text-6xl leading-normal">
+        Hello, I&apos;m{' '}
+        <span className="bg-slate-900 text-slate-100 px-2">Harish Kurma</span>
+      </div>
       <div className="text-2xl">
         Software Engineer by Profession and Passion.
       </div>
+      <a
+        className="border border-slate-900 px-6 py-4 my-16"
+        href="mailto:hkurma0504@gmail.com"
+      >
+        Contact Me
+      </a>
     </div>
   );
 };
